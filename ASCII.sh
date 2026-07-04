68839bce30f310cfe9043d94d15038fa0620113e
DCrust369 <DCrust@proton.me>
2026-07-04 23:12:39 +0100

    Create ASCII.sh

---
+++ ASCII.sh
@@ -0,0 +1,34 @@
+echo "
+                                                
+                                XXXXXXXXXXXXX                                   
+                       XXXXXXXXXX           XXXXXXXXXX                          
+                     XXX                             XXXXX                      
+                    XX                                  XX                      
+                    X XXX                            XXXXX                      
+                    X   XXXXXXX                  XXXXX   XXXXXXXXX              
+                    X         XXXXXXXXXXXXXXXXXXXX       X       XXX            
+                    X                                    X         X            
+                    X                                    X         X            
+                    X                                    X         X            
+                    X                                   XX        XX            
+                    X                                   X  XXXXXXXX             
+                    XX                                 XXXXX                    
+                     XXX                              XX                        
+                       XX                           XXX                         
+                        XXXX                    XXXXX                           
+                           XXXXXX        XXXXXXXX                               
+                                 XXXXXXXXX                                      
+
+      XXXXX XXXXXXX   XXX    XXXXXX X  XX                                       
+     XX        X     XX XX   X      X X                                         
+     XXX       X     X   X   X      XX                                          
+       XXX     X    XXXXXXX  X      X X                                         
+        XX     X    X     X  X      X  X                                        
+   XXXXXX      X    X     X  XXXXXX X   X                                       
+                                                                                
+                        XXXXXXX   XXXXXX  XXXXXXX                               
+                        X         X      XX                                     
+                        XXXXXX    XXXXX  X                                      
+                              X   X      X                                      
+                              X   X      XX                                     
+                       XXXXXXXX   XXXXXX  XXXXXXX"
