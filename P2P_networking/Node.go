0b9c97e63d3e2904724a4b50a931bbc46c659bc2
DCrust369 <DCrust@proton.me>
2026-07-04 23:05:57 +0100

    Create Node.go

---
+++ P2P_networking/Node.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main () {
+	fmt.Println("not use google or proton mail account")
+	var $SHELL = username("StackSec~$:                ")
+	var $SHELL = password("StackSec~$:                ")
+	// this is a criptografy whith a
+	// Bitfy-64
+	criptografy := ("0x1", "0x2", "0x4", "0x8","0x16", "0,32", "0x64")
+	const mul = x
+	const "0x1" = 1
+	const "0x2" = 2
+	const "0x4" = 4
+	const "0x8" = 8
+	const "0x16" = 16
+	const "0x32" = 32
+	const "0x64" = 64
+	list = ("1", "2", "4", "8", "16", "32", "64")
+	compiler_everythink = ("list", "=", "127")
+        primary_nunbers = ("2", "3", "5", "7", "11")
+	var criptografy = 7/64(0,109375*109,375)
+	var criptografy = 2/64(0,03125*128)
+	const / = div
+	var criptografy = operators[*/]("3", "5", "11")
+}
