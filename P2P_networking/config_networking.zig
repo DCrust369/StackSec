83accfb4f9683f70e54eec432a8ea8397b670afe
DCrust369 <DCrust@proton.me>
2026-07-04 23:07:01 +0100

    Create config_networking.zig

---
+++ P2P_networking/config_networking.zig
@@ -0,0 +1,35 @@
+// DCrust 26/06/2026 in the brazilian or portuguesse calendar
+const pub fn main () {
+    .const node(computer): i32 = to(computer);
+    /*
+    // i block the ISP
+    // -------------------------------------
+    // |                |                  |
+    // |                |                  |
+    // |                |                  |
+    // me              ISP see         other person
+    //--------------------------------------^
+    */
+    .const code(file): i32 = before(block)after;
+    .var before(block),
+    .var (block)after,
+    .var pass(code)>>: i32 = before(block);
+    .var criptografy: type = true;
+    // decimals
+    0x8, 0x12, 0x43, 0x54, 0x99, 0x78, 0x11, 0x33
+    1x9, 0x13, 0x44, 0x55, 0x100, 0x79, 0x13, 0x34
+    // ramdom stack
+    .const 0xerwr434,
+    .const 0x76gbgbn,
+    .const 0xert545,
+    .const 0x64beneb,
+    .const 0xr34tg6et,
+    .const 0xw456uiokjngfl
+    .const 0xryjrwge564674
+    .loop
+    .continue volatile asm () {
+        .var mov rax: i16 = 0x76gbgbn;
+        .var mov rax: i16 = 0xryjrwge564674;
+        .var mov rax: i16 = 0x64beneb;
+    }
+}
