2e5426c21f629b2dea6f0c407210be9ca72d4b91
DCrust369 <DCrust@proton.me>
2026-07-09 17:58:37 +0000

    Create 3_commands.zig

---
+++ file managenament/3_commands.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+const pub fn main () void {
+    .const sec_sel_/home/$USER/: i8 = true;
+    .var select(file): i32 = /home/$USER/;
+    if select(file) {
+        .var powershell: i8 = /home/$USERNAME$;
+    }
+    .var print("file"): i32 = printScreen("mother-file", "sub-file");
+    .const printi("archive"): i64 = printiScreen("archive-language", ".h", ".hpp", "Cmake", "Makefile", "Dockerfile", "cargo.toml", "depences");
+    .var endif_"end_process": u8[endless],
+    if CLI () {
+        .var keep_inventory: u16["true"];
+        .var leak_code: i8 = false;
+        return 0;
+        else leak_code (false) {
+            .var rust_mode: u8[true],
+            rust_mode = no_memory_leaks();
+            if rust_mode () {
+                .var dont_leaks: i64 = "rust_no_leaks"-rf(clear_commit_history(true));
+            }
+            return 0;
+        }
+        return 0;
+        pub fn (start_no_abstractions_free(true)) asm {
+            : TRUE = 1.
+            : FALSE = 0.
+            : terminal_process dq 1.
+            : mov {rax}, 0.
+            : mov {rax}, 01.
+            : mov {rax}, 001.
+            : mov {rax}, 0001.
+            : mov {rax}, 001.
+            : mov {rax}, 01.
+            : mov {rax}, 1.
+        }
+        .const mov_rax: u8[0];
+        .const mov_rdi: u8[1];
+        .const syscall,
+        return 0;
+    }
+}
