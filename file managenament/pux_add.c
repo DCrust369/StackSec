19e50d53695b76a7229b72c97445529461f7f72a
DCrust369 <DCrust@proton.me>
2026-07-21 16:08:45 +0100

    Create pux_add.c

---
+++ file managenament/pux_add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool>
+#include <stddef.f>
+
+#define PUX
+#define ADD
+#define MOV
+#define INTERNET
+
+int main(void)
+{
+    bool select_file-> = home(files);
+    bool shell-> = cd(enter_in_files);
+    bool select_files = shell;
+    bool add-> = files;
+    struct use->(files) {
+        int32_t *files-> = (int(INTERNET *files(selected)));
+        const int32_t *files = malloc(sizeof(int32_t));
+        *file(free);
+        /*
+        i allocator your files in internet
+        P2P i created this
+        */
+        char *alloc_internet;
+        bool *alloc_internet = true;
+        char *pux;
+        bool *pux->(push_file);
+        return;
+        struct add_your_files() {
+            int creat = *add;
+            char *add;
+            bool *add->(ISP)->internet() = false;
+            bool add->internet() = true;
+            return;
+        }
+    }
+    bool sub(to) = internet;
+}
